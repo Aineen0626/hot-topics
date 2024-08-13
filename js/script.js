23df4e15c8d14a2074443a4bbe56e96b93de8b09
Aineen0626 <169159845+Aineen0626@users.noreply.github.com>
2024-08-13 01:39:42 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,25 @@
+const container = document.getElementById('main-content');
+const links = document.querySelectorAll('nav a');
+let url = '';
 
+const loadContent = (urlFeed) => {
+    fetch(`partials/${urlFeed}.html`)
+    .then(response => response.text())
+    .then(data => {
+        container.innerHTML = data;
+    })
+    .catch(error => console.error('Error:', error));
+};
+
+loadContent('home');
+
+const selectContent = (event) => {
+    event.preventDefault();
+    const link = event.target;
+    const url = link.getAttribute('href');
+    loadContent(url);
+};
+
+links.forEach(link => {
+    link.addEventListener('click', selectContent);
+});
